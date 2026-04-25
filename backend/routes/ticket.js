a155f8543ee549cdf1684f1fa14c422adebd7334
thembu <rakhivhanithembu@gmail.com>
2026-04-25 13:32:10 +0200

    added route to get ticket by id

--- backend/routes/ticket.js
+++ backend/routes/ticket.js
@@ -14,6 +14,25 @@ router.get('/', async (req, res) => {
 });
 
 
+//get ticket by id
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const [rows] = await pool.query('SELECT * FROM ticket WHERE ticket_id = ?', [id]); 
+        if (rows.length === 0) {
+            return res.status(404).json({ error: 'Ticket not found' });
+        }
+        res.json(rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+//create a new ticket
+
+
+
 
 
 module.exports = router;
